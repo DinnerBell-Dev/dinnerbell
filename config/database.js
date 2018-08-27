308e311933995219dc7c3425b6e26034c1b55b77
DinnerBell-Dev <42724417+DinnerBell-Dev@users.noreply.github.com>
2018-08-27 14:57:49 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
   'secret':'nodeauthsecret',
-  'database': 'mongodb://dinnerbell:DinnerBell-2018@ds133202.mlab.com:33202/dinnerbell',
-  'test_database': 'mongodb://vasilebrinza:vasilebrinza@ds115569.mlab.com:15569/test_dinnerbell'
+  'database': 'mongodb://dinnerbell-admin:DinnerBell-2018@ds133202.mlab.com:33202/dinnerbell'
 };
