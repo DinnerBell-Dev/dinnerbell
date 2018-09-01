6b56132d04d10d2e4dd03574723b953d8cb1f7c7
DinnerBell-Dev <42724417+DinnerBell-Dev@users.noreply.github.com>
2018-08-31 23:54:28 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -16,10 +16,12 @@ router.post('/signup', function(req, res) {
   } 
   User.findOne({'email':req.body.email}, function (err, user) {
     if (user) {
+      
       // return res.json({
       //   success: false,
       //   error: constants.errors.user.duplicated_email
       // });
+      
       res.writeHeader(200, {"Content-Type": "text/html"});  
       res.write("<h2>Same account aready exist.</h2>");  
       res.end();  
@@ -32,7 +34,9 @@ router.post('/signup', function(req, res) {
         email: req.body.email,
         company_id: newRestaurant._id
       });
+      
       // save the user
+      
       newUser.save(function(err) {
         if (err) {
           console.log(err)
@@ -47,7 +51,7 @@ router.post('/signup', function(req, res) {
         //   }
         // });
         res.writeHeader(200, {"Content-Type": "text/html"});  
-        res.write("<h2>Registered Successfully, please confirm on Mobile</h2>");  
+        res.write("<h2>Registered Successfully, please confirm on your Mobile Device</h2>");  
         res.end();  
       });
     }
@@ -78,12 +82,16 @@ router.post('/signin', function(req, res) {
           });
         }
       } else {
+        
       // check if password matches
+        
         user.comparePassword(req.body.password, function (err, isMatch) {
           if (isMatch && !err) {
+            
             // if user is found and password is right create a token
             // var token = jwt.sign(user, config.secret);
             // return the information including token as JSON
+            
             if (user.status == constants.status.approved) {
               return res.json({success: true, data:{
                 restaurant_id: user.company_id
@@ -108,7 +116,7 @@ router.post('/signin', function(req, res) {
 router.post('/send_password', function(req, res) {
   var subject = 'DinnerBell recovery password.'
   var randomPass = Math.random().toString(36).substring(6);
-  var html = `Your temp password is <strong>${randomPass}</strong><br><a href='${constants.admin_site_url}'>Click here to login</a> <br> This password is available for an hour`
+  var html = `Your temporary password is <strong>${randomPass}</strong><br><a href='${constants.admin_site_url}'>Click here to login</a> <br> This temporary password is only available for one hour. Your can edit this message in USERS.JS`
 
   User.findOne({$or:[ {'customer_id': req.body.emailOrCustomerID}, {'email': req.body.emailOrCustomerID}]}, function(err, user){
     if (err) {
@@ -150,9 +158,9 @@ router.post('/send_password', function(req, res) {
 router.get('/test', function(req,res){
   var data = {
     from: constants.mailgun.from,
-    to: "nomuranori9291@gmail.com",
-    subject: "hello",
-    html: "<p>yes</p>"
+    to: "petrotalks@gmail.com",
+    subject: "DINNERBELL TEST MESSAGE",
+    html: "<p>Check USERS.JS to fix this message</p>"
   };
   console.log(data)
   mailgun.messages().send(data, function (error, body) {
